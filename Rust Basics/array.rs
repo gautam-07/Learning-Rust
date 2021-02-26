64cccccb111420a79917a4b33b3adb74f9d76b4b
Gautam <gomzekh@gmail.com>
2021-02-26 19:39:55 +0530

    Array to store same data types in Rust

---
+++ Rust Basics/array.rs
@@ -0,0 +1,25 @@
+// Array in rust
+
+// 1. A collection of multiple values
+// 2. Every element must have the same type
+// 3. Arrays in rust have a fixed length
+
+
+fn main(){
+   let months = ["January", "February", "March", "April", "May", "June", "July",    "August", "September", "October", "November", "December"];
+
+   println!("First   month = {}", months[0]);
+   println!("Second  month = {}", months[1]);
+   println!("Third   month = {}", months[2]);
+   println!("Fourth  month = {}", months[3]);
+   println!("Fifth   month = {}", months[4]);
+   println!("Sixth   month = {}", months[5]);
+   println!("Seventh month = {}", months[6]);
+   println!("Eighth  month = {}", months[7]);
+   println!("Ninth   month = {}", months[8]);
+   println!("Tenth   month = {}", months[9]);
+   println!("Eleven  month = {}", months[10]);
+   println!("Twelve  month = {}", months[11]);
+
+   // Rust do bounds checking automatically unlike languages like C and C++;
+}

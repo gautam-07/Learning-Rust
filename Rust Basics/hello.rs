025acc5ca14347d3120043eb609132bbcf1235fc
Gautam <gomzekh@gmail.com>
2021-02-26 19:38:08 +0530

    Hello World in Rust

---
+++ Rust Basics/hello.rs
@@ -0,0 +1,12 @@
+// Hello World in rust 
+
+// In rust by default variable are immutable
+// We can make them mutable by using the word "mut"
+
+fn main(){
+   let mut a = 50;
+   println!("This is a {}",a);
+   a = 60;
+   println!("This is a {}",a);
+   println!("Hello World");
+}
